5f7a8c396a25bc5e354011828fcb04b73379cd7c
Iyeba Kallon <iyebalphakallon@gmail.com>
2026-03-20 16:01:42 +0000

    Remove wasm asset extension configuration for Skia web support.
    Signed-off-by: Iyeba Kallon <iyebalphakallon@gmail.com>

--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,4 @@ const { withNativeWind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Add support for Skia on web
-config.resolver.assetExts.push("wasm");
-
 module.exports = withNativeWind(config, { input: "./global.css" });
